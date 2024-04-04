6ff64692bbe033c53a90e2df75bde41e49a95fd6
Sahand <mohsen.301933@gmail.com>
2024-04-04 20:32:21 +0330

    Implemented grid tracking arrays
    - Created arrays to monitor the numbers and colors of the grid as they are clicked.
    
    - Optimized grid monitoring using arrays
    
    - Modified boxes transition property for when they change color

--- ColorMatch.app.js
+++ ColorMatch.app.js
@@ -9,9 +9,43 @@ for (let i = 0; i < 25; i++) {
 
 // Generate random numbers for divs
 
+const divNumbers_5x5 = [];
+const divColors_5x5 = [];
 const allBoxes = document.querySelectorAll(".div");
-allBoxes.forEach(box => {
+
+function updateNumberArray() {
+    document.querySelectorAll(".div").forEach((div, index) => {
+        divNumbers_5x5[index] = div.innerHTML;
+    });
+}
+
+function AddToColorArray(color, index) {
+    if (divColors_5x5.length < index) { divColors_5x5.length = index; }
+    
+    if (color === primaryColors[0]) {
+        divColors_5x5[ index ] = "1";
+    } else if (color === primaryColors[1]) {
+        divColors_5x5[ index ] = "2";
+    } else if (color === primaryColors[2]) {
+        divColors_5x5[ index ] = "3";
+    } else if (color === primaryColors[3]) {
+        divColors_5x5[ index ] = "4";
+    } else if (color === primaryColors[4]) {
+        divColors_5x5[ index ] = "5";
+    }
+}
+
+allBoxes.forEach((box, index) => {
     box.textContent = Math.floor(Math.random() * 5) + 1;
+    updateNumberArray();
+    box.addEventListener("click", e => {
+        if (currentColor === undefined) {
+            alert("Select a color first!");
+        } else {
+            e.target.style.background = currentColor;
+            AddToColorArray(currentColor, index);
+        }
+    });
 });
 
 // Color Palette
